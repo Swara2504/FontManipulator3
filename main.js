fbca279de6f9771a08061a0128508b791e8a935a
Swara2504 <75523745+Swara2504@users.noreply.github.com>
2021-06-03 11:19:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+leftWristX= 0;
+rightWristX=0;
+difference=0;
+
+function setup()
+{
+    video= createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas= createCanvas(550,500);
+    canvas.position(560,150);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log('poseNet is initialized!');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log("results");
+        leftWristX= results[0].pose.leftWrist.x;
+        rightWristX= results[0].pose.rightWrist.x;
+        difference= floor(leftWristX - rightWristX);
+    }
+}
+function draw()
+{
+    background('#ffffb8');
+    fill('#FFD1DC');
+    text('Swara', 20,200);
+    document.getElementById("font_size").innerHTML="Font size=" + difference +"px";
+    textSize(leftWristX, rightWristX, difference);
+}
